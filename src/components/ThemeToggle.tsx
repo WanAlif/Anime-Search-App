f1834b8c5a7fb1249e2fe66ede422405eac282b2
wan <wanalifnorazlan@gmail.com>
2025-11-09 20:48:38 +0800

    Updated SearchPage

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -20,8 +20,11 @@ export const ThemeToggle: React.FC = () => {
   };
 
   return (
+    
+    
     <Button variant="ghost" size="icon" onClick={toggleTheme}>
       {theme === 'light' ? <Moon className="h-5 w-5" /> : <Sun className="h-5 w-5" />}
     </Button>
+    
   );
 };
