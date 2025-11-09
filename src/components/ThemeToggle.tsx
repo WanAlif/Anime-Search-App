8f9c69f26ccda96fdd451a0095fc90d2fe11c425
wan <wanalifnorazlan@gmail.com>
2025-11-09 22:04:29 +0800

    Toggle Update

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -22,7 +22,7 @@ export const ThemeToggle: React.FC = () => {
   return (
     
     
-    <Button variant="ghost" size="icon" onClick={toggleTheme}>
+    <Button variant ="outline" size="icon" onClick={toggleTheme}>
       {theme === 'light' ? <Moon className="h-5 w-5" /> : <Sun className="h-5 w-5" />}
     </Button>
     
